dacb355d7413a6d1c63effed1e29ad8c4d0d1e73
Ivan Burov <burov.i.v90@gmail.com>
2025-03-10 16:16:50 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Если репо называется adv_calc
+  base: '/adv_calc/',
   plugins: [react()],
 })
